8f24163cb4bca59e34222d6049831181e6a65661
Lev <levsmirnov@mail.ru>
2022-08-16 19:51:26 +0300

    Добавлено решение

--- task17/Program.cs
+++ task17/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 17. Напишите программу, которая принимает 
+//на вход координаты точки (X и Y),
+// причём X ? 0 и Y ? 0 и выдаёт номер четверти плоскости, 
+// в которой находится эта точка.
+
+//Решение:
+
+Console.WriteLine("Введите координату по X");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату по Y");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X> 0 && Y > 0)
+{
+    Console.WriteLine("Точка находится в первой четверти");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Точка находится во второй четверти");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Точка находится в третьей четверти");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Точка находится в четвертой четверти");
+}
+else
+{
+    Console.WriteLine("Мы не можем определить координатную четверть");
+}
